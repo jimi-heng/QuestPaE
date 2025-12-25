4113a5430c5871c9873e704df5d59cc42f2c55b5
吉米亨 <erzhuzi2023@outlook.com>
2025-12-25 16:34:56 +0800

    Update QuestionUI

---
+++ Assets/QuestPaE/Scripts/NextQuestionController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class NextQuestionController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] gameObjects;
+    private int num = 0;
+
+    public void ClickNext()
+    {
+        if (num < gameObjects.Length - 1)
+        {
+            gameObjects[num].SetActive(false); // 隐藏上一个问题
+            num++;
+            gameObjects[num].SetActive(true); // 显示当前问题
+        }
+    }
+
+    // 重置问题的显示
+    public void MenuReset()
+    {
+        num = 0;
+        // 显示第一个问题
+        gameObjects[0].SetActive(true);
+
+        // 隐藏剩下的问题
+        for (int i = 1; i < gameObjects.Length; i++)
+        {
+            gameObjects[i].SetActive(false);
+        }
+    }
+}
